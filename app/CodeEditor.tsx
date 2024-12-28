656f366de499d031977b07a9553ad3147ec5476d
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2024-12-28 13:02:55 +0530

    Added Code Editor Component

---
+++ app/CodeEditor.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Editor } from "@monaco-editor/react";
+import React from "react";
+
+const CodeEditor = () => {
+  return (
+    <Card className="bg-zinc-900 flex flex-col">
+      <CardHeader className="flex flex-row items-center justify-between">
+        <CardTitle className="text-white">Code Editor</CardTitle>
+        <Button className="bg-blue-800 hover:bg-blue-700"> Run Code</Button>
+      </CardHeader>
+      <CardContent className="flex-1 p-0">
+        <Editor
+          height="100vh"
+          theme="vs-dark"
+          defaultLanguage="python"
+          //   value={code}
+          options={{
+            minimap: { enabled: false },
+            fontSize: 14,
+            contextmenu: false,
+            lineNumbers: "on",
+            scrollBeyondLastLine: false,
+            automaticLayout: true,
+          }}
+        />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CodeEditor;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import React from "react";
 import Header from "./Header";
+import CodeEditor from "./CodeEditor";
 
 const page = () => {
   return (
     <div className="min-h-screen bg-zinc-950 flex flex-col">
       <Header />
+      <div className="flex-1 grid grid-cols-1 lg:grid-cols-[1fr,400px] gap-4 p-4 overflow-hidden">
+        <CodeEditor />
+      </div>
     </div>
   );
 };
