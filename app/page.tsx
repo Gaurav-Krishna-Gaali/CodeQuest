ebc8bbf8be0cbac5542c62991a9cd78227dbd24c
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2025-01-13 13:27:30 +0530

    brought out the selection state at the parent component

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,31 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import CodeEditor from "./CodeEditor";
 import Questions from "./Questions";
 import TestCases from "./TestCases";
 
-const page = () => {
+const Page = () => {
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+
   return (
     <div className="min-h-screen bg-zinc-950 flex flex-col">
       <Header />
       <div className="flex-1 grid grid-cols-1 lg:grid-cols-[2fr,1fr]  gap-4 p-4 overflow-hidden">
         <CodeEditor />
         <div className="space-y-4">
-          <Questions />
-          <TestCases />
+          <Questions
+            setSelectedQuestion={setSelectedQuestion}
+            selectedQuestion={selectedQuestion}
+          />
+          <TestCases
+            setSelectedQuestion={setSelectedQuestion}
+            selectedQuestion={selectedQuestion}
+          />
         </div>
       </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
