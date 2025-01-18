c02be7a76cbfc0382f781faf7898f5b660f4f327
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2025-01-19 00:12:41 +0530

    increading heading for large screens

--- app/Header.tsx
+++ app/Header.tsx
@@ -7,8 +7,12 @@ const Header = ({ children }: { children?: React.ReactNode }) => {
     <header className="flex items-center justify-between p-3 pl-4 border-b border-zinc-800">
       <div className="flex items-center gap-3">
         {children}
-        <code className="text-lg font-bold text-white">{"</>"}</code>
-        <span className="text-lg font-bold text-white ">CodeQuest</span>
+        <code className="text-lg lg:text-2xl font-bold text-white">
+          {"</>"}
+        </code>
+        <span className="text-lg  lg:text-2xl font-bold text-white ">
+          CodeQuest
+        </span>
       </div>
       <div className="flex items-center gap-2">
         <Login />
